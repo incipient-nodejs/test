501c7ad761deabe5d23b620484775df12aeaac7d
Bhoomil Shah <bhoomil.shah@incipientinfo.com>
2025-05-26 13:01:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/health", (req, res) => {
+  res.send("Zindha hai!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
